c2949e9a472922d6f1154d8d616a1d4d52d9d1f5
weeds <18791076614@163.com>
2020-08-25 09:06:42 +0800

    问题定位

--- carsir_test/car_soruce_list.py
+++ carsir_test/car_soruce_list.py
@@ -5,13 +5,14 @@
 @file: car_soruce_list.py
 @time: 2020/08/20 
 """
+from _thread import start_new_thread
 from time import sleep
 
 import requests
 import jsonpath
 
 
-def get_car_source_list(pageno):
+def get_car_source_list1(pageno="1"):
     data = {
         "carSourceStatus": "OFF_SHELF",
         "pageNo": pageno,
@@ -30,11 +31,48 @@ def get_car_source_list(pageno):
     return jsonpath.jsonpath(r.json(), "$.data.records.[::].carInfoId")
 
 
+def get_car_source_list2(pageno="2"):
+    data = {
+        "carSourceStatus": "OFF_SHELF",
+        "pageNo": pageno,
+        "pageSize": "10",
+        "shopId": "1272351631916408833"
+    }
+    headers = {
+        "token": "8cd2f95c0ff348a49aafed4cfe78b231",
+        "agengId": "8142f6027ad344e49e9e7cc63f34dbef",
+        "grp": "carsir_app",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url = "https://test.carsir.xin/olympic/api-olympic-admin/easySell/carSource/inStockCarSourceList"
+
+    r = requests.post(json=data, headers=headers, url=url)
+    return jsonpath.jsonpath(r.json(), "$.data.records.[::].carInfoId")
+
+
+def get_my_car_source_list():
+    data = {
+        "carSourceStatus": "IN_STOCK",
+        "pageNo": "1",
+        "pageSize": "10",
+        "shopId": "1272351631916408833"
+    }
+    headers = {
+        "token": "8cd2f95c0ff348a49aafed4cfe78b231",
+        "agengId": "8142f6027ad344e49e9e7cc63f34dbef",
+        "grp": "carsir_app",
+        "Content-Type": "application/json; charset=utf-8"
+    }
+    url = "https://test.carsir.xin/olympic/api-olympic-admin/easySell/carSource/inStockCarSourceList"
+
+    r = requests.post(json=data, headers=headers, url=url)
+
+
 def in_detail():
     url = "https://test.carsir.xin/olympic/api-olympic-admin/easySell/carSource/carSourceDetails"
     data = {
         "shopId": "1272351631916408833",
-        "carInfoId": "8eafffb7988a44de91f0ff711a0dae29"
+        "carInfoId": "7121ff6060df4650a52cee3ce37ade9a"
     }
     headers = {
         "token": "8cd2f95c0ff348a49aafed4cfe78b231",
@@ -48,17 +86,19 @@ def in_detail():
 def run_list():
     number = 0
     for i in range(20):
-        list3 = get_car_source_list("1")
+        list3 = get_car_source_list1()
         in_detail()
-        list1 = get_car_source_list("1")
+
+        start_new_thread(get_my_car_source_list,())
+        list1 = start_new_thread(get_car_source_list1, ())
         sleep(1)
-        list2 = get_car_source_list("2")
+        list2 = get_car_source_list2()
         print(f"list1; {list1}")
         print(f"list2; {list2}")
-        if list2[0] in list1:
-            number += 1
-        if list1 != list3:
-            number += 1
+        # if list2[0] in list1:
+        #     number += 1
+        # if list1 != list3:
+        #     number += 1
     print(number)
 
 
